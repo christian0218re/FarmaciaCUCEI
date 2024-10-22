20c4a14062986b3394344e246e8ce7cb5e17c3c4
PrayerOne1 <160205286+PrayerOne1@users.noreply.github.com>
2024-10-21 23:23:50 -0600

    Product.py is already finished, just some validations and combobox
    Product.py is already functioning, just some validations, check Ventas.py is starting before the login

--- Productos.py
+++ Productos.py
@@ -1,244 +1,238 @@
-# Productos.py
 from baseDatos import conectar
 import tkinter as tk
-from tkinter import ttk, messagebox
-from tkinter import messagebox
+from tkinter import messagebox, ttk
+
 
 def createProductWindow():
-    # Función para crear un nuevo producto
-    def crear_producto():
+    productWindow = tk.Tk()
+    productWindow.title("Gestión de Productos")
+
+    def buscar_producto(codigoID, codeEntry, descEntry, priceEntry, stockEntry, nameEntry, idProv):
         conn = conectar()
         cursor = conn.cursor()
-
-        # Obtener los valores de los campos de entrada
-        productoId = idEntry.get()
-        nombre = nameEntry.get()
-        descripcion = descriptionEntry.get()
-        precio = precioEntry.get()
-        stock = stockEntry.get()
-
-        # Obtener el proveedor seleccionado del combobox
-        proveedor_nombre = proveedorCombo.get()
-        proveedorId = proveedores_dict.get(proveedor_nombre)
-
-        # Validaciones
-        try:
-            precio = float(precio)  # Asegurarse de que el precio es un número decimal
-            stock = int(stock)  # Asegurarse de que el stock es un número entero
-        except ValueError:
-            messagebox.showinfo("Error", "El precio debe ser un número decimal y el stock un número entero")
-            return
-
-        if precio <= 0:
-            messagebox.showinfo("Error", "El precio debe ser mayor a cero")
-            return
-
-        if stock < 0:
-            messagebox.showinfo("Error", "El stock no puede ser negativo")
-            return
+        codigo = codigoID.get()
 
         try:
-            # Verificar que el productoId no esté en uso
-            cursor.execute("SELECT * FROM productos WHERE productoId = ?", (productoId,))
-            if cursor.fetchone():
-                messagebox.showinfo("Error", "El ID del producto ya está registrado")
-                return
-
-            if nombre and descripcion:  # Validar que nombre y descripción estén llenos
-                cursor.execute("""
-                    INSERT INTO productos (productoId, nombre, descripcion, precio, stock, proveedorId)
-                    VALUES (?, ?, ?, ?, ?, ?)
-                """, (productoId, nombre, descripcion, precio, stock, proveedorId))
-                conn.commit()
-                messagebox.showinfo("Éxito", "El producto ha sido creado correctamente")
-                cleanProductWindow()
-            else:
-                messagebox.showinfo("Error", "Por favor rellene los campos Nombre y Descripción antes de continuar")
-        except Exception as e:
-            messagebox.showinfo("Error", str(e))
-        finally:
-            conn.close()
-
-    # Función para obtener la lista de proveedores
-    def obtener_proveedores():
-        conn = conectar()
-        cursor = conn.cursor()
-
-        # Obtener la lista de proveedores (nombre y proveedorId)
-        cursor.execute("SELECT proveedorId, nombre FROM proveedores")
-        proveedores = cursor.fetchall()
-        conn.close()
-
-        # Crear un diccionario de proveedorId y nombre
-        return {proveedor[1]: proveedor[0] for proveedor in proveedores}
-
-    # Función para buscar un producto
-    def buscar_producto():
-        conn = conectar()
-        cursor = conn.cursor()
-        buscarProducto = idSearch.get()
-
-        try:
-            cursor.execute("SELECT * FROM productos WHERE productoId = ?", (buscarProducto,))
+            cursor.execute(
+                "SELECT productoId, nombre, descripcion, precio, stock, proveedorId FROM productos WHERE productoId = ?",
+                (codigo,))
             producto = cursor.fetchone()
-
             if producto:
-                # Llenar los campos del formulario con los datos del producto
-                idEntry.delete(0, tk.END)
-                idEntry.insert(0, producto[0])
+                codeEntry.delete(0, tk.END)
+                codeEntry.insert(0, producto[0])
+
                 nameEntry.delete(0, tk.END)
                 nameEntry.insert(0, producto[1])
-                descriptionEntry.delete(0, tk.END)
-                descriptionEntry.insert(0, producto[2])
-                precioEntry.delete(0, tk.END)
-                precioEntry.insert(0, producto[3])
+
+                descEntry.delete(0, tk.END)
+                descEntry.insert(0, producto[2])
+
+                priceEntry.delete(0, tk.END)
+                priceEntry.insert(0, producto[3])
+
                 stockEntry.delete(0, tk.END)
                 stockEntry.insert(0, producto[4])
 
-                # Convertir proveedorId en nombre de proveedor para el combobox
-                proveedorId = producto[5]
-                for nombre, id in proveedores_dict.items():
-                    if id == proveedorId:
-                        proveedorCombo.set(nombre)
-                        break
+                idProv.delete(0, tk.END)
+                idProv.insert(0, producto[5])
+
             else:
-                messagebox.showinfo("Error", "Producto no encontrado")
+                messagebox.showerror("Error", "Producto no encontrado.")
         except Exception as e:
-            messagebox.showinfo("Error", str(e))
+            messagebox.showerror("Error de base de datos", str(e))
         finally:
             conn.close()
 
-    # Función para actualizar un producto
-    def actualizar_producto():
-        conn = conectar()
-        cursor = conn.cursor()
-        productoId = idEntry.get()
+    def agregar_producto(codeEntry, descEntry, priceEntry, stockEntry, tree, nameEntry, idProv):
+        codigo = codeEntry.get()
+        descripcion = descEntry.get()
         nombre = nameEntry.get()
-        descripcion = descriptionEntry.get()
-        precio = precioEntry.get()
-        stock = stockEntry.get()
-
-        # Obtener el proveedor seleccionado del combobox
-        proveedor_nombre = proveedorCombo.get()
-        proveedorId = proveedores_dict.get(proveedor_nombre)
 
         try:
-            precio = float(precio)
-            stock = int(stock)
+            precio = float(priceEntry.get())
+            stock = int(stockEntry.get())
         except ValueError:
-            messagebox.showinfo("Error", "El precio debe ser un número decimal y el stock un número entero")
+            messagebox.showerror("Error", "El precio o el stock no es válido.")
             return
 
-        if not productoId or not nombre or not descripcion or not precio or not stock or not proveedorId:
-            messagebox.showinfo("Error", "Por favor rellene todos los campos antes de continuar")
-            return
-
-        # Verificar que el producto exista
-        cursor.execute("SELECT * FROM productos WHERE productoId = ?", (productoId,))
-        if not cursor.fetchone():
-            messagebox.showinfo("Error", "El producto no existe")
-            return
-
-        try:
-            cursor.execute("""
-                UPDATE productos
-                SET nombre = ?, descripcion = ?, precio = ?, stock = ?, proveedorId = ?
-                WHERE productoId = ?
-            """, (nombre, descripcion, precio, stock, proveedorId, productoId))
-            conn.commit()
-
-            messagebox.showinfo("Éxito", "Producto actualizado correctamente")
-            cleanProductWindow()
-        except Exception as e:
-            messagebox.showinfo("Error", str(e))
-        finally:
-            conn.close()
-
-    def eliminar_producto():
-        conn = conectar()
-        cursor = conn.cursor()
-        productoId = idEntry.get()
-
-        try:
-            cursor.execute("DELETE FROM productos WHERE productoId = ?", (productoId,))
-            conn.commit()
-            messagebox.showinfo("Éxito", "Producto eliminado correctamente")
-            cleanProductWindow()
-        except Exception as e:
-            messagebox.showinfo("Error", str(e))
-        finally:
-            conn.close()
-
-    def getCurrentID():
         conn = conectar()
         cursor = conn.cursor()
 
-        # Obtener el último ID de la base de datos de productos
+        try:
+            cursor.execute(
+                "INSERT INTO productos (productoId, nombre, descripcion, precio, stock, proveedorId) VALUES (?, ?, ?, ?, ?, ?)",
+                (codigo, nombre, descripcion, precio, stock, idProv.get()))
+            conn.commit()
+            messagebox.showinfo("Éxito", "Producto agregado correctamente.")
+            cleanProductWindow()
+
+            # Agregar el producto a la tabla
+            tree.insert("", "end",
+                        values=(False, codigo, nombre, descripcion, precio, stock))  # checkbox state is False
+
+        except Exception as e:
+            messagebox.showerror("Error", f"Error al agregar el producto: {e}")
+        finally:
+            conn.close()
+
+    def getCurrentId():
+        conn = conectar()
+        cursor = conn.cursor()
+
+        # Obtener el último ID de la base de datos
         cursor.execute("SELECT MAX(productoId) FROM productos")
         last_id = cursor.fetchone()[0]
 
         # Si no hay productos, el ID inicial será 1, de lo contrario será el siguiente al último ID
         next_id = 1 if last_id is None else last_id + 1
+        return next_id
 
-        cleanProductWindow()
-        idEntry.insert(0, next_id)
+    def modificar_producto(codeEntry, descEntry, priceEntry, stockEntry, nameEntry, idProv):
+        codigo = codeEntry.get()
+        descripcion = descEntry.get()
+        nombre = nameEntry.get()
+        idProvValue = idProv.get()
+        try:
+            precio = float(priceEntry.get())
+            stock = int(stockEntry.get())
+        except ValueError:
+            messagebox.showerror("Error", "El precio o el stock no es válido.")
+            return
 
-        conn.close()
+        conn = conectar()
+        cursor = conn.cursor()
+        if codigo and descripcion and nombre and idProvValue:
+            try:
+                cursor.execute(
+                    "UPDATE productos SET nombre = ?, descripcion = ?, precio = ?, stock = ?, proveedorId = ? WHERE productoId = ?",
+                    (nombre, descripcion, precio, stock, idProvValue, codigo))
+                conn.commit()
+                messagebox.showinfo("Éxito", "Producto modificado correctamente.")
+                cleanProductWindow()
+            except Exception as e:
+                messagebox.showerror("Error", f"Error al modificar el producto: {e}")
+            finally:
+                conn.close()
+        else:
+            messagebox.showerror("Error", "Llene los campos correspondientes antes de intentar editar un producto")
 
-    # Función para limpiar los campos
+    def eliminar_producto(codigoID, tree):
+        codigo = codigoID.get().strip()  # Get the input and remove leading/trailing whitespace
+
+        # Validate that the input is not empty
+        if not codigo:
+            messagebox.showerror("Error", "El código no puede estar vacío.")
+            return  # Exit the function early if the input is empty
+
+        # Check if all relevant entry fields are empty
+        if all(field.get().strip() == "" for field in [codigoID, nameEntry, descEntry, priceEntry, stockEntry, idProv]):
+            messagebox.showerror("Error", "Todos los campos están vacíos. Por favor, complete al menos uno.")
+            return  # Exit if all fields are empty
+
+        conn = conectar()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("DELETE FROM productos WHERE productoId = ?", (codigo,))
+            conn.commit()
+            messagebox.showinfo("Éxito", "Producto eliminado correctamente.")
+            cleanProductWindow()  # Assuming this function clears the input fields
+            # Eliminar de la tabla
+            for item in tree.get_children():
+                if tree.item(item, 'values')[1] == codigo:
+                    tree.delete(item)
+                    break
+        except Exception as e:
+            messagebox.showerror("Error", f"Error al eliminar el producto: {e}")
+        finally:
+            conn.close()
+
+    def set_new_product_id():
+        next_id = getCurrentId()
+        codeEntry.delete(0, tk.END)
+        codeEntry.insert(0, next_id)
+
+    def toggle_checkbox(event):
+        item = tree.identify_row(event.y)
+        if item:
+            current_value = tree.item(item, "values")[0]
+            new_value = not current_value if current_value else True  # Toggle checkbox state
+            tree.item(item, values=(new_value,) + tree.item(item, "values")[1:])
+
+    def delete_selected_products():
+        for item in tree.get_children():
+            if tree.item(item, "values")[0]:  # Check if checkbox is selected
+                # Delete the product from the database
+                codigo = tree.item(item, "values")[1]
+                eliminar_producto(codigoID=tk.Entry(productWindow, text="codigo"), tree=tree)
+                # Remove the item from the tree
+                tree.delete(item)
+
     def cleanProductWindow():
-        idEntry.delete(0, tk.END)
+        codeEntry.delete(0, tk.END)
         nameEntry.delete(0, tk.END)
-        descriptionEntry.delete(0, tk.END)
-        precioEntry.delete(0, tk.END)
+        descEntry.delete(0, tk.END)
+        priceEntry.delete(0, tk.END)
         stockEntry.delete(0, tk.END)
-        proveedorCombo.set('')  # Limpiar el combobox de proveedores
+        idProv.delete(0, tk.END)
 
-    # Crear la ventana principal para gestionar productos
-    product_window = tk.Tk()
-    product_window.title("Productos")
-    product_window.geometry("650x590")
+        # User Interface setup
+    tk.Label(productWindow, text='Código').grid(row=0, column=0)
+    tk.Label(productWindow, text="Nombre").grid(row=1, column=0)
+    tk.Label(productWindow, text='Descripción').grid(row=2, column=0)
+    tk.Label(productWindow, text='Precio').grid(row=3, column=0)
+    tk.Label(productWindow, text='Stock').grid(row=1, column=2)
+    tk.Label(productWindow, text="ID proveedor").grid(row=2, column=2)
 
-    # Etiquetas y entradas para buscar productos por ID
-    tk.Label(product_window, text='Ingrese el ID').grid(row=0, column=0, padx=10, pady=10)
-    idSearch = tk.Entry(product_window)
-    idSearch.grid(row=0, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Buscar', command=buscar_producto).grid(row=0, column=2, padx=10, pady=10)
+    codeEntry = tk.Entry(productWindow)
+    codeEntry.grid(row=0, column=1)
+    nameEntry = tk.Entry(productWindow)
+    nameEntry.grid(row=1, column=1)
+    descEntry = tk.Entry(productWindow)
+    descEntry.grid(row=2, column=1)
+    priceEntry = tk.Entry(productWindow)
+    priceEntry.grid(row=3, column=1)
+    stockEntry = tk.Entry(productWindow)
+    stockEntry.grid(row=1, column=3)
+    idProv = tk.Entry(productWindow)
+    idProv.grid(row=2, column=3)
 
-    # Etiquetas para los campos del producto
-    tk.Label(product_window, text='ID Producto').grid(row=1, column=0, padx=10, pady=10)
-    tk.Label(product_window, text='Nombre del Producto').grid(row=2, column=0, padx=10, pady=10)
-    tk.Label(product_window, text='Descripción').grid(row=3, column=0, padx=10, pady=10)
-    tk.Label(product_window, text='Precio').grid(row=4, column=0, padx=10, pady=10)
-    tk.Label(product_window, text='Stock').grid(row=5, column=0, padx=10, pady=10)
-    tk.Label(product_window, text='Proveedor').grid(row=6, column=0, padx=10, pady=10)
+    codigoID = tk.Entry(productWindow)
+    tk.Label(productWindow, text='Buscar código').grid(row=0, column=2)
+    codigoID.grid(row=0, column=3)
+    tk.Button(productWindow, text='Buscar',
+            command=lambda: buscar_producto(codigoID, codeEntry, descEntry, priceEntry, stockEntry, nameEntry,
+                                                  idProv)).grid(row=0, column=4)
 
-    # Entradas para los campos del producto
-    idEntry = tk.Entry(product_window)
-    idEntry.grid(row=1, column=1, padx=10, pady=10)
-    nameEntry = tk.Entry(product_window)
-    nameEntry.grid(row=2, column=1, padx=10, pady=10)
-    descriptionEntry = tk.Entry(product_window)
-    descriptionEntry.grid(row=3, column=1, padx=10, pady=10)
-    precioEntry = tk.Entry(product_window)
-    precioEntry.grid(row=4, column=1, padx=10, pady=10)
-    stockEntry = tk.Entry(product_window)
-    stockEntry.grid(row=5, column=1, padx=10, pady=10)
+    # Botones de acción
+    tk.Button(productWindow, text="Agregar Producto",
+            command=lambda: agregar_producto(codeEntry, descEntry, priceEntry, stockEntry, tree, nameEntry,
+                                            idProv)).grid(row=4, column=1)
+    tk.Button(productWindow, text="Modificar Producto",
+                command=lambda: modificar_producto(codeEntry, descEntry, priceEntry, stockEntry, nameEntry,
+                                                     idProv)).grid(row=4, column=2)
+    tk.Button(productWindow, text="Eliminar Producto", command=lambda: eliminar_producto(codigoID, tree)).grid(
+            row=4, column=3)
 
-    # Menú desplegable para seleccionar proveedor
-    proveedores_dict = obtener_proveedores()  # Obtener diccionario de proveedores
-    proveedorCombo = ttk.Combobox(product_window, values=list(proveedores_dict.keys()))
-    proveedorCombo.grid(row=6, column=1, padx=10, pady=10)
+    tk.Button(productWindow, text="Nuevo", command=set_new_product_id).grid(row=4, column=4)
 
-    # Botones para las acciones de los productos
-    tk.Button(product_window, text='Nuevo', width=20, command=getCurrentID).grid(row=7, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Actualizar', width=20, command=actualizar_producto).grid(row=8, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Guardar', width=20, command=crear_producto).grid(row=9, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Eliminar', width=20, command=eliminar_producto).grid(row=10, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Cancelar', width=20, command=cleanProductWindow).grid(row=11, column=1, padx=10, pady=10)
-    tk.Button(product_window, text='Salir', width=20, command=product_window.destroy).grid(row=0, column=3, padx=10, pady=10)
+    # Tabla para mostrar productos
+    tree = ttk.Treeview(productWindow, columns=("checkbox", "codigo", "nombre", "descripcion", "precio", "stock"),
+                            show='headings')
+    tree.heading("checkbox", text="")
+    tree.heading("codigo", text="Código")
+    tree.heading("nombre", text="Nombre")
+    tree.heading("descripcion", text="Descripción")
+    tree.heading("precio", text="Precio")
+    tree.heading("stock", text="Stock")
+    tree.grid(row=5, column=0, columnspan=5, padx=10, pady=10)
 
-    product_window.mainloop()
+    tree.column("checkbox", width=50, anchor="center")
 
-    
+    # Bind the checkbox toggle to mouse click
+    tree.bind("<Button-1>", toggle_checkbox)
 
+    # Add a button to delete selected products
+    tk.Button(productWindow, text="Eliminar productos seleccionados", command=delete_selected_products).grid(row=6,
+                                                                                                             column=4)
+    productWindow.mainloop()
